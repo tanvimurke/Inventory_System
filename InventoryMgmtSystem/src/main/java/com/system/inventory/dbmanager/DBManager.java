9ee12edb41d1551c1334b7c023489ac215b638b3
nilesh1168 <n.suryawanshi1168@gmail.com>
2020-10-30 18:43:10 +0530

    changes in pom

--- InventoryMgmtSystem/src/main/java/com/system/inventory/dbmanager/DBManager.java
+++ InventoryMgmtSystem/src/main/java/com/system/inventory/dbmanager/DBManager.java
@@ -12,7 +12,7 @@ public class DBManager {
 	public static Connection getConnection() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/inventory","root","");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/inventory","root","nilesh@8101998");
 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
